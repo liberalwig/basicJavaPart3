94dab199a5ccd4286ebddb943be6b3676cac9168
최승은 <liberalfilm@gmail.com>
2022-10-28 19:29:26 +0900

    2022-10-28-Fri 19:29

---
+++ src/main/java/ch02/VIPCustomer.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class VIPCustomer extends Customer{
+    double salesRatio;
+    private String agentID;
+
+    public String getAgentID() {
+        return agentID;
+    }
+
+    public VIPCustomer() {
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+    }
+}
